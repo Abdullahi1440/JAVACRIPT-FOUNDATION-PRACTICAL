85e45435dd3c2ff61c79303985f7637876fe2ff1
Abdullahi <guure440@gmail.com>
2021-10-10 11:40:14 +0300

    addition of condition from untrac to stage

---
+++ conditional.js
@@ -0,0 +1,72 @@
+// objects
+// let person=new Object();
+// // person.name="mohamud";
+// // person.age=30;
+// // console.log(person)
+// let user={magaca:"mohamud cali" ,
+// age:20,
+// course:"html",
+// price:12,
+// arday:true
+
+// }// displaying
+// console.log(user.age)
+// array----> of objects
+// let courses=[
+    
+//     {
+//         name:"css",
+
+//         price:30,
+//      "arday ma yahay":true,
+
+//     }   
+// ]
+// console.log(courses[0]['arday ma yahay'])
+
+
+
+// //loops  --> while | for | do while loop
+// let n =0;
+// while(n<5){
+// n+=2
+//     console.log(n);
+//     // break
+// }// infinite loop that cause jumping
+// n1=0;
+// do{
+//     n1++
+//     console.log(n1)
+//     // break;
+// }
+// while(n1<5);
+
+// //for loop
+// let data=["abdi","Aisha",15,22,true]
+// // console.log(data[])
+// // let num;
+// // // for(let num=0; num<=data.length; num++){
+// for(let d of data){
+//     console.log(d)
+// // }
+// let person={
+//     name:"Abdifitah",
+//     number:988774747,
+//     age:20
+
+// }
+// for(let ob in person){
+//     console.log(person[ob])
+// }
+//function of javascript
+//function always return something
+//to call function use the word function
+function getAge(){//camil case
+    //the body of the function
+    console.log(40)
+}
+getAge(20)
+getAge(30)
+
+    
+
